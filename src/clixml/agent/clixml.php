8f8d5ed4c213ef6a8dccf7ab8f622e11a89f4f47
Shaheem Azmal M MD <shaheem.azmal@siemens.com>
2016-08-08 18:26:53 +0530

    feat(clixmltest): Implemented testcases for clixml agent

--- src/clixml/agent/clixml.php
+++ src/clixml/agent/clixml.php
@@ -15,7 +15,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-namespace Fossology\clixml;
+namespace Fossology\CliXml;
 
 use Fossology\Lib\Agent\Agent;
 use Fossology\Lib\Dao\UploadDao;
