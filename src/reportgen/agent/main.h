4a6285e4a23426cd2a9a9512ebb70176da550225
Anupam Ghosh <anupam.ghosh@siemens.com>
2018-04-24 12:12:30 +0530

    fix(reportgen): remove json-c ambiguity

--- src/reportgen/agent/main.h
+++ src/reportgen/agent/main.h
@@ -14,7 +14,7 @@
  with this program; if not, write to the Free Software Foundation, Inc.,
  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-#include <json/json.h>
+#include <json-c/json.h>
 #include <stdlib.h>
 #include <time.h>
 
